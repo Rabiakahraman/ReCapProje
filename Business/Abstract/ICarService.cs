f6c3593cfdb9600c9c7494c5993c444d0325d081
Rabia <acer@ACER-BILGISAYAR>
2021-02-23 21:42:37 +0300

    8,odev

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -8,5 +8,7 @@ namespace Business.Abstract
     public interface ICarService
     {
         List<Car> GetAll();
+        List<Car> GetCarByBrandId(int id);
+        List<Car> GetCarByColorId(int id);
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -15,10 +17,34 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+        public void Add(Car car)
+        {
 
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                if (car.DailyPrice > 0 && context.Brands.SingleOrDefault(b => b.BrandId == car.BrandId).BrandName.Length > 2)
+                {
+                    _carDal.Add(car);
+                }
+                else
+                {
+                    Console.WriteLine("Ekleme işleminiz yapılamamıştır.");
+                }
+
+            }
+        }
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+        public List<Car> GetCarByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
         }
     }
 }
